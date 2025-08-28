fcc44570fb0a2ed18a5d3c0d9384a3351811676a
OnTheRocksEvents <cocktelerianocheydia@gmail.com>
2025-08-29 00:02:18 +0200

    Create margalida.py
    Añadido módulo Margalida

---
+++ agents/margalida.py
@@ -0,0 +1,6 @@
+class Margalida:
+    def __init__(self):
+        self.role = "Coaching & Bienestar"
+
+    def recomendar_actividad(self, estado_emocional):
+        return f"Actividad recomendada para mejorar {estado_emocional}"
