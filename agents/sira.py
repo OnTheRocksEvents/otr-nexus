fe6ed2c90a2361727b498c0980d7499d09f97eca
OnTheRocksEvents <cocktelerianocheydia@gmail.com>
2025-08-29 00:01:35 +0200

    Create sira.py
    Añadido módulo Sira

---
+++ agents/sira.py
@@ -0,0 +1,6 @@
+class Sira:
+    def __init__(self):
+        self.role = "Automatización & Fichajes"
+
+    def automatizar_tarea(self, tarea):
+        return f"Tarea '{tarea}' automatizada con éxito"
