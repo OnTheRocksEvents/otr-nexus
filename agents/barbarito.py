c7bd8ee78d826226097da82de0386dc207fb7dc2
OnTheRocksEvents <cocktelerianocheydia@gmail.com>
2025-08-29 00:03:02 +0200

    Create barbarito.py
    Añadido módulo Barbarito

---
+++ agents/barbarito.py
@@ -0,0 +1,6 @@
+class Barbarito:
+    def __init__(self):
+        self.role = "Logística & Producción"
+
+    def planificar_entrega(self, destino):
+        return f"Entrega planificada para {destino}"
